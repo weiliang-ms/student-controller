40da321b4e1c5fd7a8ff26175166a89dd24e72a7
weiliang <weilaing@neusoft.com>
2019-11-26 15:00:21 +0800

    调整包名

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -26,9 +26,9 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
 bash "${CODEGEN_PKG}"/generate-groups.sh "deepcopy,client,informer,lister" \
- $GOPATH/src/student-controller/pkg/generated student-controller/pkg/apis \
+  student-controller/pkg/generated student-controller/pkg/apis \
   wlcontroller:v1 \
-  --output-base "$(dirname "${BASH_SOURCE[0]}")/../../.." \
+  --output-base "$GOPATH/src" \
   --go-header-file "${SCRIPT_ROOT}"/hack/boilerplate.go.txt
 
 # To use your own boilerplate text append:
