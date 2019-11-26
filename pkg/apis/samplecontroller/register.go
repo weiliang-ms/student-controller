8642f53da3bb50e9466a9e60b2cdce63ceb6b264
weiliang <weilaing@neusoft.com>
2019-11-26 11:07:35 +0800

    调整包名

--- pkg/apis/samplecontroller/register.go
+++ pkg/apis/samplecontroller/register.go
@@ -3,5 +3,5 @@ package samplecontroller
 // 定义全局变量
 const (
 	GroupName = "samplecontroller.k8s.io"
-	Version   = "v1alpha1"
+	Version   = "v1"
 )
--- pkg/apis/samplecontroller/v1alpha1/doc.go
+++
@@ -1,4 +0,0 @@
-// +k8s:deepcopy-gen=package
-
-// +groupName=samplecontroller.k8s.io
-package v1alpha1
